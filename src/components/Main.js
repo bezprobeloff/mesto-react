4580d659dfbdb9da27bbfad29eac3893bbeabcd7
Mikhail Belov <bezprobeloff@gmail.com>
2022-07-27 09:41:02 +0300

    feat: добавлены императивные обработчики открытия попапов

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,35 @@
 import avatar from '../images/avatar.jpg';
 
 const Main = () => {
+  const handleEditAvatarClick = () => {
+    const popup = document.querySelector('.popup_type_update-avatar');
+    popup.classList.add('popup_opened');
+  };
+
+  const handleEditProfileClick = () => {
+    const popup = document.querySelector('.popup_type_edit-profile');
+    popup.classList.add('popup_opened');
+  };
+
+  const handleAddPlaceClick = () => {
+    const popup = document.querySelector('.popup_type_add-card');
+    popup.classList.add('popup_opened');
+  };
+
   return (
     <main class="content">
       <section class="profile" aria-label="Профиль пользователя">
-        <div class="profile__avatar-container">
+        <div class="profile__avatar-container"  onClick={handleEditAvatarClick}>
           <img src={avatar} alt="Аватар пользователя" class="profile__avatar-image"/>
         </div>
         <div class="profile__info">
           <div class="profile__container-name">
             <h1 class="profile__name">User</h1>
-            <button class="profile__button-edit" type="button" aria-label="Редактировать"></button>
+            <button class="profile__button-edit" type="button" onClick={handleEditProfileClick} aria-label="Редактировать"></button>
           </div>
           <p class="profile__job">About</p>
         </div>
-        <button class="profile__button-add" type="button" aria-label="Добавить"></button>
+        <button class="profile__button-add" type="button" onClick={handleAddPlaceClick} aria-label="Добавить"></button>
       </section>
       <section class="cards" aria-label="Фотокарточки"></section>
     </main>
