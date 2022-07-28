ceee9cabc9893be653268d233122c25058bcefcd
Mikhail Belov <bezprobeloff@gmail.com>
2022-07-28 09:22:02 +0300

    fix: тег на отключение валидации по умолчанию

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -6,7 +6,7 @@ const PopupWithForm = (props) => {
     <div className={`popup popup_type_${props.name} ${classPopupOpened}`}>
       <div className="popup__container">
         <button className="popup__button-close" onClick={props.onClose} type="button" aria-label="Закрыть"></button>
-        <form className="popup__form" name={`form-${props.name}`} action="form" method="post" novalidate>
+        <form className="popup__form" name={`form-${props.name}`} action="form" noValidate>
           <h3 className="popup__title">{props.title}</h3>
 
           <button className="popup__button popup__button_type_submit" type="submit">Сохранить</button>
