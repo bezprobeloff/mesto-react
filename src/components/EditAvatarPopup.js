affb64fbcb79af034e0a642693ce52eac0e54ae2
Mikhail Belov <bezprobeloff@gmail.com>
2022-08-14 20:58:56 +0300

    feat: неуправляющий компонент по обновлению аватара

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -1,30 +1,30 @@
 import PopupWithForm from "./PopupWithForm";
-import { useState, useEffect, useRef, useContext } from 'react';
-import { CurrentUserContext } from '../contexts/CurrentUserContext';
+import { useRef, useEffect } from 'react';
 
 const EditAvatarPopup = ({isOpen, onClose, onUpdateAvatar}) => {
-  const currentUser = useContext(CurrentUserContext);
   const inputAvatarRef = useRef();
-  const [avatar, setAvatar] = useState('');
 
   useEffect(() => {
-    setAvatar(inputAvatarRef.name);
-  }, [currentUser]);
+    inputAvatarRef.value = '';
+  }, [isOpen]);
 
   const handleChangeAvatar = (evt) => {
-
+    inputAvatarRef.value = evt.target.value;
   };
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
+
+    onUpdateAvatar(inputAvatarRef.value);
   };
+
   return (
     <PopupWithForm title='Обновить аватар' name='update-avatar'
     isOpen={isOpen} onClose={onClose} onSubmit={handleSubmit}>
       <>
       <input className='popup__input popup__input_type_avatar'
-        type='url' name={avatar} ref={inputAvatarRef} id='avatar'
-        placeholder='Ссылка на аватарку' minLength='2' required/>
+        type='url' name='avatar' ref={inputAvatarRef} onChange={handleChangeAvatar}
+        id='avatar' placeholder='Ссылка на аватарку' minLength='2' required/>
       <span className='popup__input-error popup__input-error_type_avatar'></span>
       </>
     </PopupWithForm>
